3ee17c576ebd6cafed8608ba61103cb6a2229c7e
Enes Kazazić <enes.kazazic@edu.fit.ba>
2022-06-11 13:30:18 +0200

    url fix

--- eRestoran/eRestoran.WinUI/Properties/Resources.Designer.cs
+++ eRestoran/eRestoran.WinUI/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace eRestoran.WinUI.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://localhost:5001/api/.
+        ///   Looks up a localized string similar to http://localhost:5001/api/.
         /// </summary>
         internal static string ApiURL {
             get {
